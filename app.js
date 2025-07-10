f880a1e8c1f9e284ee1ad840b943acc51bdff3c9
vath1605 <chanvathana1208@gmail.com>
2025-07-10 18:44:15 +0700

    Delete

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const displayStock = () => {
     `;
     display.innerHTML = output;
   } else {
-    stock.map((val) => {
+    stock.map((val,index) => {
       if (
         val.name != null &&
         val.qty != null &&
@@ -55,8 +55,8 @@ const displayStock = () => {
 </svg>
                 </button>
                 <button type="button" onclick="deleteProduct(${
-                  val.code
-                })" class="btn btn-danger">
+                  index
+                })" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteProduct">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
 </svg>
@@ -148,4 +148,18 @@ const viewProduct=(code)=>{
       modal.innerHTML = output;
     }
   })
+}
+const deleteProduct =(index)=>{
+  const yesBtn = document.getElementById('yesBtn');
+  const imgContainer = document.getElementById('imgContainer');
+  let img = stock[index].image;
+  imgContainer.innerHTML = `
+    <img class="w-25" src="${img}" alt="">
+  `;
+  yesBtn.onclick=()=>{
+    stock.splice(index,1);
+    displayStock();
+
+    document.getElementById('noBtn').click();
+  }
 }
